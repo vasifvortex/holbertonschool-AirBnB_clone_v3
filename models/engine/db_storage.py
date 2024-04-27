b15d65d92abc4f15f6312789bb24b6161d1aff1d
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-28 03:06:51 +0400

    Some stats?

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,86 +1,140 @@
-#!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
 
+#!/usr/bin/python3
+import os
+import sys
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.base_model import Base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 from models.amenity import Amenity
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy.orm import scoped_session
-import os
+
+USER = os.getenv("HBNB_MYSQL_USER")
+PASSWORD = os.getenv("HBNB_MYSQL_PWD")
+HOST = os.getenv("HBNB_MYSQL_HOST")
+DB = os.getenv("HBNB_MYSQL_DB")
 
 
 class DBStorage:
-    """This class manages storage of hbnb models"""
+    """
+    This class represents the database storage for the AirBnB clone project.
+    It provides methods to interact with the database, such as retrieving,
+    creating, updating, and deleting objects.
+    """
+
     __engine = None
     __session = None
 
-    def __init__(self) -> None:
-        """Creates a new FileStorage instance"""
-
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(os.getenv('HBNB_MYSQL_USER'),
-                                              os.getenv('HBNB_MYSQL_PWD'),
-                                              os.getenv('HBNB_MYSQL_HOST'),
-                                              os.getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
-        if os.getenv('HBNB_ENV') == 'test':
+    def __init__(self):
+        """
+        Initializes a new instance of the DBStorage class.
+        It creates a database engine and drops all tables if the 'test'
+        argument
+        is present in the sys.argv list.
+        """
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DB),
+            pool_pre_ping=True,
+        )
+        if "test" in sys.argv:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
-        if cls:
-            objects = self.__session.query(cls).all()
+        """
+        Retrieves all objects from the database.
+
+        Args:
+            cls (optional): The class of objects to retrieve. If not provided,
+                            all objects from all classes will be retrieved.
+
+        Returns:
+            A dictionary of objects, where the key is in the format
+            "<class_name>.<object_id>" and the value is the object itself.
+        """
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
-            classes = [User, State, City, Amenity, Place, Review]
-            objects = []
-            for cls in classes:
-                objects += self.__session.query(cls).all()
-
-        result = {}
-        for obj in objects:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            result[key] = obj
-
-        return result
-
-    def get(self, cls, id):
-        all = self.all(cls)
-        for key in list(all.keys()):
-            if key == f"{cls.__name__}.{id}":
-                return all[f"{key}"]
-        return None
-
-    def count(self, cls=None):
-            return len(self.all(cls))
+            if type(cls) is str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(obj).__name__, obj.id): obj
+                for obj in objs}
 
     def new(self, obj):
-        """Adds new object to storage"""
+        """
+        Adds a new object to the database session.
+
+        Args:
+            obj: The object to add to the session.
+        """
         self.__session.add(obj)
 
     def save(self):
-        """Saves storage to file"""
+        """
+        Commits the changes made in the current session to the database.
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Deletes obj from storage"""
+        """
+        Deletes an object from the database session.
+
+        Args:
+            obj (optional): The object to delete. If not provided, no action
+                            will be taken.
+        """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Loads storage from file"""
+        """
+        Reloads the database session and creates all tables defined in the
+        metadata.
+        """
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def get(self, cls, id):
+        """
+        Retrieves an object from the database.
+
+        Args:
+            cls: The class of the object to retrieve.
+            id: The id of the object to retrieve.
+
+        Returns:
+            The object if found, None otherwise.
+        """
+        return self.__session.query(cls).get(id)
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in the database storage.
+
+        Args:
+            cls (optional): The class name of the objects to count.
+            If not provided, counts all objects.
+
+        Returns:
+            int: The number of objects in the database storage.
+        """
+        return len(self.all(cls))
 
     def close(self):
-        """Call remove() method on the private session attribute"""
+        """
+        Closes the current database session.
+        """
         self.__session.close()
-        return self.__session

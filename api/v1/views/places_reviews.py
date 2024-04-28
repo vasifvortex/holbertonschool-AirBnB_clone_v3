bfc65c4378061a8c6405f561083d4ccec04973a2
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-28 18:36:07 +0400

    Seymur’s commit: Review class was handled

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+'''Doc for reviews'''
+from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
+from models.review import Review
+from models.place import Place
+from models.user import User
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET'],
+                 strict_slashes=False)
+def get_reviews_by_place_id(place_id):
+    '''Doc for reviews'''
+    place = storage.get(Place, place_id)
+    if place is None:
+        return jsonify({"error": "Not found"}), 404
+    reviews = [review.to_dict() for review in place.reviews]
+    return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def get_review_by_id(review_id):
+    '''Doc for reviews'''
+    review = storage.get(Review, review_id)
+    if review is None:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(review.to_dict())
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_review(review_id):
+    '''Doc for reviews'''
+    review = storage.get(Review, review_id)
+    if review is None:
+        return jsonify({"error": "Not found"}), 404
+    storage.delete(review)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def post_review(place_id):
+    '''Doc for reviews'''
+    place = storage.get(Place, place_id)
+    if place is None:
+        return jsonify({"error": "Not found"}), 404
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'user_id' not in data:
+        return jsonify({"error": "Missing user_id"}), 400
+    if storage.get(User, data['user_id']) is None:
+        return jsonify({"error": "Not found"}), 404
+    if 'text' not in data:
+        return jsonify({"error": "Missing text"}), 400
+    data['place_id'] = place_id
+    review = Review(**data)
+    review.save()
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+def put_review(review_id):
+    '''Doc for reviews'''
+    review = storage.get(Review, review_id)
+    if review is None:
+        return jsonify({"error": "Not found"}), 404
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    for key, value in data.items():
+        if key not in ['id', 'user_id', 'place_id', 'created_at',
+                       'updated_at']:
+            setattr(review, key, value)
+    review.save()
+    return jsonify(review.to_dict()), 200
